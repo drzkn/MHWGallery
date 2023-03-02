0f793f7141819c239a9ebec94ad957474c63f81d
drzkn <diegorazq@gmail.com>
2023-03-02 16:14:42 +0100

    config: añade los ficheros de configuracion de tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
+  darkMode: true,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
